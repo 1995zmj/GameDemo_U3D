327d786c0739611cf57d14c90b2d45c35b55afb2
1995zmj <minjiangzhao@icloud.com>
2020-01-19 18:33:16 +0800

    更新

---
+++ Assets/Nucleon/Nucleon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    public float attractionForce;
+    private Rigidbody body;
+    private void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        body.AddForce(transform.localPosition * - attractionForce);
+    }
+}
