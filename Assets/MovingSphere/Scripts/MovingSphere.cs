6c6e84cf81fd2263176900bcd5140d97ae3c54b5
1995zmj <minjiangzhao@icloud.com>
2020-01-19 20:21:06 +0800

    更新

---
+++ Assets/MovingSphere/Scripts/MovingSphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 displacement = new Vector3(playerInput.x, 0f, playerInput.y);
+        transform.localPosition += displacement;
+    }
+}
