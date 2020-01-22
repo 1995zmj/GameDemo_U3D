be9ea45fdab3407429166c59f16a155ce921a7d5
1995zmj <minjiangzhao@icloud.com>
2020-01-22 19:56:30 +0800

    更新

--- Assets/MovingSphere/Scripts/MovingSphere.cs
+++ Assets/MovingSphere/Scripts/MovingSphere.cs
@@ -4,20 +4,140 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+    // [SerializeField]
+    // Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    // [SerializeField, Range(0f,1f)]
+    // float bounciness = 0.5f;
+
+    Rigidbody body;
+    Vector3 velocity, desiredVelocity;
+    bool desiredJump = false;
+    bool onGround;
+    int jumpPhase;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        
+        velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 playerInput;
+        desiredJump |= Input.GetButtonDown("Jump");
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 displacement = new Vector3(playerInput.x, 0f, playerInput.y);
-        transform.localPosition += displacement;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        // Vector3 displacement = velocity * Time.deltaTime;
+
+        // Vector3 newPosition = transform.localPosition + displacement;
+
+        // if(newPosition.x < allowedArea.xMin)
+        // {
+        //     newPosition.x = allowedArea.xMin;
+        //     velocity.x = - velocity.x * bounciness;
+        // }
+        // else if(newPosition.x > allowedArea.xMax)
+        // {
+        //     newPosition.x = allowedArea.xMax;
+        //     velocity.x = - velocity.x * bounciness;
+        // }
+
+        // if(newPosition.z < allowedArea.yMin)
+        // {
+        //     newPosition.z = allowedArea.yMin;
+        //     velocity.z = - velocity.z * bounciness;
+        // }
+        // else if(newPosition.z > allowedArea.yMax)
+        // {
+        //     newPosition.z = allowedArea.yMax;
+        //     velocity.z = - velocity.z * bounciness;
+        // }
+        // transform.localPosition = newPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateState();
+        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+
+        body.velocity = velocity;
+        onGround = false;
+    }
+
+    private void UpdateState()
+    {
+        velocity = body.velocity;
+        if (onGround)
+        {
+            jumpPhase = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            if(velocity.y > 0f)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+            }
+            velocity.y += jumpSpeed;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        onGround = true;
+        EvaluateCollision(other);
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        onGround = true;
+        EvaluateCollision(other);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        onGround = false;
+    }
+
+    private void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= 0.9f;
+        }
     }
 }
---
+++ Assets/PersistingObjects/Game.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform prefab;
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+
+    private List<Transform> objects;
+    // Start is called before the first frame update
+    private string savePath;
+    private void Awake() {
+        objects = new List<Transform>();
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+        else if(Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }else if(Input.GetKeyDown(saveKey))
+        {
+            Save();
+        }
+    }
+
+    private void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f,1f);
+        objects.Add(t);
+    }
+
+    private void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    private void Save()
+    {
+        using (var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+            writer.Write(objects.Count);
+            for (int i = 0; i < objects.Count; i++)
+            {
+                Transform t = objects[i];
+                writer.Write(t.localPosition.x);
+                writer.Write(t.localPosition.y);
+                writer.Write(t.localPosition.z);
+            }
+        }
+    }
+    
+    private void Load()
+    {
+        using (var reader = new BinaryReader(File.Open(savePath, FileMode.Open)))
+        {
+            int cout = reader.ReadInt32();
+        }
+    }
+}
